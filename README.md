a83600b594dada9403e6709c25f4e1d1aa59c7fe
lilv98 <zhenwei.tang@kaust.edu.sa>
2022-12-08 09:01:43 +0300

    update_readme_and_examples

--- README.md
+++ README.md
@@ -28,26 +28,7 @@ Language Modeling for Neural Multi-hop Logical Reasoning over Knowledge Graphs
 
 ## Run
 
-- Reproducing Query Type Generalization Performance on NELL995 
-    * GQE
-        > `CUDA_VISIBLE_DEVICES=0 nohup python main.py --cuda --do_train --do_valid --do_test --data_path ./private_data -n 128 -b 512 -d 800 -g 24 -lr 0.0001 --max_steps 450001 --cpu_num 1 --geo vec --valid_steps 15000 --tasks "1p.2p.3p.4p.5p.2i.3i.4i.5i.ip.pi.2u.up" --print_on_screen >vec.log 2>&1 &`
-
-    * Query2Box
-        > `CUDA_VISIBLE_DEVICES=0 nohup python main.py --cuda --do_train --do_valid --do_test --data_path ./private_data -n 128 -b 512 -d 400 -g 60 -lr 0.0001 --max_steps 450001 --cpu_num 1 --geo beta --valid_steps 15000 -betam "(1600,2)" --tasks "1p.2p.3p.4p.5p.2i.3i.4i.5i.ip.pi.2u.up" --print_on_screen >beta.log 2>&1`
-    
-    * BetaE
-        > `CUDA_VISIBLE_DEVICES=0 nohup python main.py --cuda --do_train --do_valid --do_test --data_path ./private_data -n 128 -b 512 -d 400 -g 24 -lr 0.0001 --max_steps 450001 --cpu_num 1 --geo box --valid_steps 15000 -boxm "(none,0.02)" --tasks "1p.2p.3p.4p.5p.2i.3i.4i.5i.ip.pi.2u.up" --print_on_screen >box.log 2>&1 &`
-
-- Reproducing Language Model Enhanced Reasoning Performance on NELL995 
-    * GQE
-        > `CUDA_VISIBLE_DEVICES=0 nohup python main.py --cuda --do_train --do_valid --do_test --data_path ./private_data -n 128 -b 512 -d 800 -g 24 -lr 0.0001 --max_steps 450001 --cpu_num 1 --geo vec --valid_steps 15000 --tasks "1p.2p.3p.4p.5p.2i.3i.4i.5i.ip.pi.2u.up" --word_embed_path ./private_data/emb128.pkl --print_on_screen >vec_lm.log 2>&1 &`
-    
-    * Query2Box
-        > `TODO: PY`
-
-    * BetaE
-        > `TODO: PY`
-
+- To reproduce the Query Type Generalization and Language Model Enhanced Reasoning experimental results on NELL995, please refer to the commands provided in `examples.sh`.
 
 ## Acknowledgement
 This repo is based on the official implementation of `GQE`, `Query2Box`, and `BetaE` provided at `https://github.com/snap-stanford/KGReasoning`.
--- example.sh
+++ example.sh
@@ -1,13 +1,23 @@
-# old examples
-#CUDA_VISIBLE_DEVICES=0 nohup python main.py --cuda --do_train --do_valid --do_test --data_path ./private_data -n 128 -b 512 -d 800 -g 24 -lr 0.0001 --max_steps 450001 --cpu_num 1 --geo vec --valid_steps 15000 --tasks "1p.2p.3p.4p.5p.2i.3i.4i.5i.ip.pi.2u.up" --print_on_screen >vec.log 2>&1 &
-#
-#CUDA_VISIBLE_DEVICES=1 nohup python main.py --cuda --do_train --do_valid --do_test --data_path ./private_data -n 128 -b 512 -d 400 -g 60 -lr 0.0001 --max_steps 450001 --cpu_num 1 --geo beta --valid_steps 15000 -betam "(1600,2)" --tasks "1p.2p.3p.4p.5p.2i.3i.4i.5i.ip.pi.2u.up" --print_on_screen >beta.log 2>&1 &
-#
-#CUDA_VISIBLE_DEVICES=0 nohup python main.py --cuda --do_train --do_valid --do_test --data_path ./private_data -n 128 -b 512 -d 400 -g 24 -lr 0.0001 --max_steps 450001 --cpu_num 1 --geo box --valid_steps 15000 -boxm "(none,0.02)" --tasks "1p.2p.3p.4p.5p.2i.3i.4i.5i.ip.pi.2u.up" --print_on_screen >box.log 2>&1 &
-#
-#CUDA_VISIBLE_DEVICES=0 python main.py --cuda --do_train --do_valid --do_test --data_path ./private_data -n 128 -b 512 -d 800 -g 24 -lr 0.0001 --max_steps 450001 --cpu_num 1 --geo vec --valid_steps 15000 --tasks "1p.2p.3p.4p.5p.2i.3i.4i.5i.ip.pi.2u.up" --word_embed_path ./private_data/emb128.pkl --print_on_screen
+# box without word_embed
+CUDA_VISIBLE_DEVICES=0 python main.py \
+--cuda --do_train --do_valid --do_test --data_path ./private_data \
+-n 128 -b 512 -d 800 -g 24 -lr 0.0001 \
+--max_steps 450001 --cpu_num 1 \
+--geo box --valid_steps 15000 -boxm "(none,0.02)" \
+--valid_steps 15000 \
+--tasks "1p.2p.3p.4p.5p.2i.3i.4i.5i.ip.pi.2u.upgi" \
+--print_on_screen
 
-# new version
+## vec with word_embed
+CUDA_VISIBLE_DEVICES=0 python main.py \
+--cuda --do_train --do_valid --do_test --data_path ./private_data \
+-n 128 -b 512 -d 800 -g 24 -lr 0.0001 \
+--max_steps 450001 --cpu_num 1 --geo vec --valid_steps 15000 \
+--tasks "1p.2p.3p.4p.5p.2i.3i.4i.5i.ip.pi.2u.up" \
+--use_wb --word_embed_path ./private_data/emb128.pkl \
+--print_on_screen
+
+# vec without word_embed
 CUDA_VISIBLE_DEVICES=0 python main.py \
 --cuda --do_train --do_valid --do_test --data_path ./private_data \
 -n 128 -b 512 -d 800 -g 24 -lr 0.0001 \
@@ -15,3 +25,23 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
 --tasks "1p.2p.3p.4p.5p.2i.3i.4i.5i.ip.pi.2u.up" \
 --print_on_screen
 
+# beta with word_embed
+CUDA_VISIBLE_DEVICES=0 python main.py \
+--cuda --do_train --do_valid --do_test --data_path ./private_data \
+-n 128 -b 2 -d 800 -g 24 -lr 0.0001 \
+--max_steps 450001 --cpu_num 1 \
+--geo beta --valid_steps 15000 -betam "(1600,2)" \
+--valid_steps 15000 \
+--tasks "1p.2p.3p.4p.5p.2i.3i.4i.5i.ip.pi.2u.up" \
+--use_wb --word_embed_path ./private_data/emb128.pkl \
+--print_on_screen
+
+# beta without word_embed
+CUDA_VISIBLE_DEVICES=0 python main.py \
+--cuda --do_train --do_valid --do_test --data_path ./private_data \
+-n 128 -b 512 -d 800 -g 24 -lr 0.0001 \
+--max_steps 450001 --cpu_num 1 \
+--geo beta --valid_steps 15000 -betam "(1600,2)" \
+--valid_steps 15000 \
+--tasks "1p.2p.3p.4p.5p.2i.3i.4i.5i.ip.pi.2u.upgi" \
+--print_on_screen
